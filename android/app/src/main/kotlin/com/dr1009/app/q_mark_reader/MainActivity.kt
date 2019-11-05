ad5ba11747705a34b7b5340d2cc4c4e14e2ed22e
Koji Wakamiya <koji.wakamiya@gmail.com>
2019-11-05 23:30:59 +0900

    Create Project

---
+++ android/app/src/main/kotlin/com/dr1009/app/q_mark_reader/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.dr1009.app.q_mark_reader
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
